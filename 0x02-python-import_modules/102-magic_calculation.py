2fd43ba4679c95e60db4c9c976358b07ff764d1e
shadowclan-R <rayanabdulazizm.ali@gmail.com>
2023-12-04 19:02:32 -0800

    updatde

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Match bytecode provided"""
+    """will Match bytecode provided"""
     from magic_calculation_102 import add, sub
 
     if a < b:
